ffa0d1c9b7490b5be6f505c82c1b3813b30f208f
Alexander <alexlimay27@gmail.com>
2022-08-18 23:39:53 -0500

    cliente

---
+++ src/main/java/com/idat/EfAlexanderChinininClienteApplication.java
@@ -0,0 +1,13 @@
+package com.idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfAlexanderChinininClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfAlexanderChinininClienteApplication.class, args);
+	}
+
+}
